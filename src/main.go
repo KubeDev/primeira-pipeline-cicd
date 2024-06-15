752c6d4d342e3a54344409ebb070b46d6d1b3932
fsv-aula <fsv.aula@gmail.com>
2024-06-15 11:29:03 -0300

    v2.0

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo")
+	fmt.Fprintf(w, "Aplicacao exemplo - v2.0")
 }
 
 func main() {
